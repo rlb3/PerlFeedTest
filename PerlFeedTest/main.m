b47f205701da8badecff9e4f0dfea9b42555ccfa
Robert Boone <robo4288@gmail.com>
2011-12-14 09:56:05 -0600

    The real main.m

--- PerlFeedTest/main.m
+++ PerlFeedTest/main.m
@@ -7,16 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <PubSub/PubSub.h>
 
 int main (int argc, const char * argv[])
 {
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+    NSURL *url = [NSURL URLWithString:@"http://search.cpan.org/uploads.rdf"];
+    
+    NSURLRequest  *request = [NSURLRequest requestWithURL:url];
+    NSURLResponse *response;
+    NSError *error;
+    NSData  *perlRssData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        
+    PSFeed *feed = [[PSFeed alloc] initWithData:perlRssData URL:url];
 
+    // [feed refresh:&error];
+
+    // Go through each entry and print out the title, authors, and content
+    for (PSEntry *entry in [feed entries]) {
+        
+        printf("Entry Title:%s\n", [entry.titleForDisplay UTF8String]);
+        printf("Entry Authors:%s\n", [entry.authorsForDisplay UTF8String]);
+        printf("Entry Content:%s\n\n", [entry.content.plainTextString UTF8String]);
+    }
+    
+    [feed release];
     [pool drain];
     return 0;
 }
-
